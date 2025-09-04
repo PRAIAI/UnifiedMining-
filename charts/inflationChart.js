87b265f993acc304da515cd07b8f34ba14eb8adb
Planet Rescuer Axiometikx Intelligence Artificial Intelligence (@PR-A-I) <planet.rescuer.ai@gmail.com>
2025-09-04 18:10:20 +0200

    Create inflationChart.js

---
+++ charts/inflationChart.js
@@ -0,0 +1,5 @@
+export function renderInflationChart(containerId) {
+  const container = document.getElementById(containerId);
+  container.innerHTML = "<div style='color:cyan;'>InterBOxSpider@Web.NET Inflationschart wird geladen...</div>";
+  // Hier kann TradingView oder D3.js eingebunden werden
+}
